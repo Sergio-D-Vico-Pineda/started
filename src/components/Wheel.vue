cd95bc943b41a9d85fcd0420232717bab83cdd0e
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-01-24 17:32:50 +0100

    props fix props msg

--- src/components/Wheel.vue
+++ src/components/Wheel.vue
@@ -3,10 +3,21 @@
    <h2 v-else>Ruedas del {{ name }}</h2>
    <p v-if="num > 1">Tiene {{ num }} ruedas</p>
    <p v-else-if="num == 1">Tiene {{ num }} rueda</p>
+   <button @click="printMsg(msg)">Ruedas print</button>
 </template>
 
 <script>
 export default {
-   props: ['name', 'num', 'fem']
+   props:
+   {
+      printMsg: Function,
+      name: String,
+      num: Number,
+      fem: Boolean
+   },
+   setup() {
+      let msg = "Wheel.vue";
+      return { msg };
+   }
 }
 </script>
